eacfb08c952363ed940b12e7669defad154be75b
Victoria-Imaobong <solomonvictoria2023@gmail.com>
2026-02-18 20:00:48 +0100

    patient service

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class    }) // This is what triggers the green arrow
+@SpringBootApplication  // This is what triggers the green arrow
 public class PatientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientServiceApplication.class, args);
